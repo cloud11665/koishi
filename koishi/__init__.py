2469b28fd689eb33b3a5ce7c2c5fc173066cd77d
Bartek Sabat <Cloud11665@gmail.com>
2021-03-06 23:31:39 +0100

    add: add methods, rewrite contests

--- koishi/__init__.py
+++ koishi/__init__.py
@@ -5,30 +5,31 @@ from typing import Dict, List, Optional, Union
 from urllib.error import HTTPError, URLError
 
 import chardet
-from lxml import etree, html
-from markdownify import markdownify as markdown
-from requests import Session, request
+import lxml
+import markdownify as md
+import requests
 
-import models as mod
+from models import CStatus, statdict
 from contest import Contest
 from exceptions import AccessError, LoginError
 
+from utils import xpstr
+from utils import parse_news
 
 class Koishi:
-	def __init__(self, username:str, password:str):
-		self.uname = username
+	def __init__(self, login:str, password:str):
+		self.login = login
 		self.passwd = password
-		self.ses = Session() # To store the session cookie
+		self.ses = requests.Session() # Stores the session cookie
 
-		resp = self.ses.post("https://satori.tcs.uj.edu.pl/login",
-			data = {
-				"login": self.uname,
-				"password": self.passwd
-			},
-			files = [] # To simulate a multipart/data request
+		resp = self.ses.post(
+			"https://satori.tcs.uj.edu.pl/login",
+			files = (
+				("login", (None, self.login)),
+				("password", (None, self.passwd))
 			)
-
-		dom = html.fromstring(resp.text)
+		)
+		dom = lxml.html.fromstring(resp.text)
 
 		if not resp.ok:
 			raise HTTPError(resp.url,resp.status_code,"Internal satori error",
@@ -38,15 +39,12 @@ class Koishi:
 			raise HTTPError(resp.url,resp.status_code,"Invalid login credentials",
 				hdrs=None,fp=None)
 
-		name = dom.xpath("//ul[@class='headerRightUl']/li[1]/text()")
-		name = "".join(name).split()[-2:] # "Logged in as {1} {2}" -> ["{1}", "{2}"]
-		self.name = name
 
 	@property
-	@lru_cache(2<<16)
+	@lru_cache(None)
 	def contests(self) -> List[Contest]:
 		resp = self.ses.get("https://satori.tcs.uj.edu.pl/contest/select")
-		dom = html.fromstring(resp.text)
+		dom = lxml.html.fromstring(resp.text)
 
 		out = []
 		for table in dom.xpath("//table[@class='results']"):
@@ -63,7 +61,7 @@ class Koishi:
 
 				status = tr.xpath(".//td[3]//text()")
 				status = "".join(status)
-				status = mod.CStatus(mod.statdict[status])
+				status = CStatus(statdict[status])
 
 				out.append(Contest(self.ses,
 						id=id_, name=name, desc=desc, status=status
@@ -71,64 +69,93 @@ class Koishi:
 
 		return out
 
-	@singledispatchmethod
-	@lru_cache(2<<16)
-	def problems(self, con:Contest):
-		if con.status.value != 1:
-			raise AccessError(f"account not a member of \"{con.name}\" contest")
+	# @problems.register
+	# @lru_cache(2<<10)
+	# def _(self, con:int):
+	# 	con = [x for x in self.contests if x.id == con]
+	# 	if con:
+	# 		return self.problems(*con)
 
-		resp = self.ses.get(f"https://satori.tcs.uj.edu.pl/contest/{con.id}/problems")
-		dom = html.fromstring(resp.text)
+	# 	raise ValueError("specified id not found")
 
-		if resp.status_code == 404:
-			raise HTTPError(resp.url,resp.status_code,"Contest not found",
+	@property
+	@lru_cache(None)
+	def news(self) -> List[str]:
+		""""""
+		resp = self.ses.get(f"https://satori.tcs.uj.edu.pl/news")
+		return parse_news(resp.text)
+
+	@property
+	def profile(self) -> Dict[str, str]:
+		resp = self.ses.get("https://satori.tcs.uj.edu.pl/profile")
+		dom = lxml.html.fromstring(resp.text)
+
+		fname = xpstr(dom, "//input[@id='id_firstname']/@value")
+		lname = xpstr(dom, "//input[@id='id_lastname']/@value")
+		affi  = xpstr(dom, "//input[@id='id_affiliation']/@value")
+
+		return {
+			"first_name": fname,
+			"last_name": lname,
+			"affiliation": affi,
+			}
+
+	def relog(self) -> bool:
+		resp = self.ses.post(
+			"https://satori.tcs.uj.edu.pl/login",
+			files = (
+				("login", (None, self.login)),
+				("password", (None, self.passwd))
+			)
+		)
+		return resp.ok
+
+	def update(self, **fields) -> None:
+		"""Updates user's profile.
+
+		Updates users satori profile with specified keyword arguments.
+		When no value is provided, parameter's value will not be updated.
+
+		Args:
+			first_name:
+				A string representing the new first name of the user.
+			last_name:
+				A string representing the new last name of the user.
+			password:
+				A string representing the new password of the user.
+			affiliation:
+				A string representing the new affiliation of the user.
+
+		Raises:
+			urllib.error.HTTPError: An error occurred while updating the profile.
+		"""
+		fname = self.profile["first_name"]
+		lname = self.profile["last_name"]
+		affi  = self.profile["affiliation"]
+
+		fname = fields.get("first_name", fname)
+		lname = fields.get("last_name", lname)
+		affi = fields.get("affiliation", affi)
+
+		newpass = fields.get("password", "")
+		oldpass = self.passwd if newpass else ""
+
+		resp = self.ses.post(
+			"https://satori.tcs.uj.edu.pl/profile",
+			files=(
+				("firstname", (None, fname)),
+				("lastname", (None, lname)),
+				("oldpass", (None, oldpass)),
+				("newpass", (None, newpass)),
+				("confirm", (None, newpass)),
+				("affiliation", (None, affi)),
+				("changepass", (None, "change"))
+			)
+		)
+
+		if not resp.ok:
+			raise HTTPError(resp.url,resp.status_code,
+				"An error occurred while updating the profile",
 				hdrs=None,fp=None)
 
-		out = []
-		for series in zip(
-			dom.xpath("//div[@id='content']/h4"),
-			dom.xpath("//div[@id='content']/div")
-		):
-			title_, tasks_ = series
-
-			title = title_.xpath(".//text()")
-			title = "".join(title)
-			title = re.sub("^\s+|\s+.$|\s*\[\w+\/\w+\]\s*$", "", title)
-			# "{1} [show/hide] " -> "{1}"
-			# "{1} " -> "{1}"
-			# " {1}" -> "{1}"
-
-			tasks = []
-			for tr in tasks_.xpath(".//table[@class='results']/tr[position()!=1]"):
-				code = tr.xpath(".//td[1]/text()")
-				code = "".join(code)
-
-				name = tr.xpath(".//td[2]/a/text()")
-				name = "".join(name)
-
-				id_ = tr.xpath(".//td[2]/a/@href")
-				id_ = "".join(id_)
-				id_ = re.sub("^\/\w+\/\d+\/\w+\/|\/$", "", id_)
-
-				note = tr.xpath(".//*[@class='mainsphinx']/p/em/text()")
-				note = "".join(note)
-
-				tasks.append(mod.Problem(
-					code=code, id=id_, note=note, name=name, parent_id=con.id
-				))
-
-			out.append(mod.ProblemSet(
-				title=title, problems=tasks
-			))
-
-		return out
-
-	@problems.register
-	@lru_cache(2<<10)
-	def _(self, con:int):
-		con = [x for x in self.contests if x.id == con]
-		if con:
-			return self.problems(*con)
-
-		raise ValueError("specified id not found")
-
+		return self.profile.values() == [fname, lname, affi]
